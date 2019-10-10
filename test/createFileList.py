29b87f857a3ba28695d0b8025335445e64b7f024
Paul Asmuss <paul.asmuss@cern.ch>
2019-10-10 16:23:00 +0200

    needs (QCD Madgraph samples)

--- test/createFileList.py
+++ test/createFileList.py
@@ -1,22 +1,25 @@
 import os
 
-basedir = '/pnfs/desy.de/cms/tier2/store/user/rwalsh/Analysis/Ntuples/DATA/Run2017/ntuplizer_94X_2017_rereco31Mar18-v4/JetHT/'
-prefix = 'Run2017'
-suffix = '-31Mar2018-v1/'
+basedir = '/pnfs/desy.de/cms/tier2/store/user/rwalsh/Analysis/Ntuples/MC/Fall17/ntuplizer_94X_mc_2017_fall17-v4/'
+prefix = 'QCD_HT'
+suffix = '_TuneCP5_13TeV-madgraph-pythia8/'
 
-eras = ['C','D','E','F']
+eras = ['200to300','300to500','500to700','700to1000','1000to1500','1500to2000','2000toInf']
 
 for era in eras:
     ntuplelist = []
     print(era)
-    with open('filelists/rootFileListJetHT_Run2017'+str(era)+'.txt','w') as filelist:
-        times = os.listdir(basedir+prefix+str(era)+suffix)
-        for time in times:
-            timepath = basedir+prefix+str(era)+suffix+time+'/'
-            parts = os.listdir(timepath)
-            for part in parts:
-                partpath = timepath+part+'/'
-                ntuples = os.listdir(partpath)
-                for ntuple in ntuples:
-                    ntuplepath = partpath+ntuple+'\n'
-                    filelist.write(ntuplepath)
+    with open('filelists/rootFileList_MadgraphQCDHT'+str(era)+'.txt','w') as filelist:
+        folders = os.listdir(basedir+prefix+str(era)+suffix)
+        for folder in folders:
+            folderpath = basedir+prefix+str(era)+suffix+folder+'/'
+            times = os.listdir(folderpath)
+            for time in times:
+                timepath = folderpath+time+'/'
+                parts = os.listdir(timepath)
+                for part in parts:
+                    partpath = timepath+part+'/'
+                    ntuples = os.listdir(partpath)
+                    for ntuple in ntuples:
+                        ntuplepath = partpath+ntuple+'\n'
+                        filelist.write(ntuplepath)
